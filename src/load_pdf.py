ce1b62e13854e56e33018605fb61bfa341791e8f
Nithees Kanna <nithees.kanna@ptechnosoft.com>
2026-01-14 18:48:36 +0530

    Refactor PDF loading to use PyMuPDFLoader
    Updated PDF loading functionality to use PyMuPDFLoader for improved performance and error handling.

--- src/load_pdf.py
+++ src/load_pdf.py
@@ -1,12 +1,47 @@
-from pypdf import PdfReader #importing the Pdfreader package
+import os
+from langchain_community.document_loaders import PyMuPDFLoader
 
 
-def load_pdf_text(pdf_path):
-    reader = PdfReader(pdf_path)
-    full_text=""
-    for page_number, page in enumerate(reader.pages): #enumerate provided the page number and content and is assigned to the page_number and page variables
-        text = page.extract_text()
-        if text:
-            full_text += f"\n--- Page {page_number + 1} ---\n" #assigning the page number to the text and so making the chunking process easy
-            full_text += text
-        return full_text
+def load_pdf_file(file_path: str):
+    # 1. Check if file exists first
+    if not os.path.exists(file_path):
+        print(f"Error: The file '{file_path}' was not found.")
+        return []
+
+    print(f"--- Loading {file_path} ---")
+
+    try:
+        # PyMuPDF is optimized for speed and complex layouts for reading the PDF
+        loader = PyMuPDFLoader(file_path)
+        docs = loader.load()
+
+        # 2. Check if the PDF was empty or readable
+        if not docs:
+            print("Error: PDF loaded but no text found")
+            return []
+
+        print(f"Successfully loaded {len(docs)} pages.")
+        return docs
+
+    except Exception as e:
+        print(f"Unexpected Error loading PDF: {e}")
+        return []
+
+
+"""
+******************************************
+LOAD MODULE HAS BEEN TESTED AND CONFIGURED
+******************************************
+
+if __name__ == "__main__":
+    # Create a dummy PDF or point to an existing one to test
+    test_pdf = "D:\RAG LANGCHAIN PROJECT\data\data_file111.pdf"  # Make sure this file exists in your project folder
+
+    if not os.path.exists(test_pdf):
+        print(f" Please put a PDF named '{test_pdf}' in this folder to test.")
+    else:
+        loaded_docs = load_pdf_file(test_pdf)
+        if loaded_docs:
+            print(f"Test Preview: {loaded_docs[0].page_content[:200]}...")
+
+"""
