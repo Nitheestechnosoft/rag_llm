aa3d21894689ebd93f1ddbae30ed5c6684ddf162
Nithees Kanna <nithees.kanna@ptechnosoft.com>
2026-01-14 18:48:12 +0530

    Refactor text splitting to use RecursiveCharacterTextSplitter
    Updated the text splitting logic to use RecursiveCharacterTextSplitter for improved chunking of documents.

--- src/chunker.py
+++ src/chunker.py
@@ -1,15 +1,60 @@
-def split_text(text, chunk_size=800, overlap=100):
-#logic starts here
-    chunks = []
-    start = 0
-    text_length = len(text)
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
-    while start < text_length:
-        end = start + chunk_size
-        chunk = text[start:end]
+def split_documents(docs):
+    """
+    """
+    print("--- Splitting Documents ---")
 
-        chunks.append(chunk.strip())
+    # Configure the splitter
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=350,  # Size of each chunk in characters
+        chunk_overlap=80,  # Overlap to keep context between chunks
+        length_function=len,
+        is_separator_regex=False,
+    )
 
-        start += chunk_size - overlap
+    # Perform the split
+    splits = text_splitter.split_documents(docs)
 
-    return chunks
+    if not splits:
+        print(" Error: No chunks created.")
+        return []
+
+    print(f" Split {len(docs)} pages into {len(splits)} chunks.")
+
+    return splits
+
+"""
+
+**********************************
+CHUNK MODULE VERIFIED AND EXECUTED
+**********************************
+
+# --- TEST BLOCK ---
+# Run this file directly to test if splitting works
+if __name__ == "__main__":
+    # We need to import the loader from Step 1 to test this
+    from load_pdf import load_pdf_file
+    import os
+
+    test_pdf = "D:\RAG LANGCHAIN PROJECT\data\data_file111.pdf"  # Ensure this file exists
+
+
+
+
+    if os.path.exists(test_pdf):
+        # 1. Load
+        raw_docs = load_pdf_file(test_pdf)
+
+        # 2. Split (Testing this module)
+        if raw_docs:
+            my_chunks = split_documents(raw_docs)
+
+            # Show the first chunk content to verify
+            print("\n--- Preview of First Chunk ---")
+            print(my_chunks[0].page_content)
+            print("------------------------------")
+    else:
+        print(f"File '{test_pdf}' not found. Please add it to test.")
+
+"""
